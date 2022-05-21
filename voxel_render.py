caeb21a4f212d49407ccab982e4ed9cbfa505375
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-05-21 17:09:40 +0300

    add ray casting

--- voxel_render.py
+++ voxel_render.py
@@ -13,6 +13,39 @@ map_height = len(height_map[0])
 map_width = len(height_map)
 
 
+@njit(fastmath=True)
+def ray_casting(screen_array, player_pos, player_angle, player_height, player_pitch,
+                screen_width, screen_height, delta_angle, ray_distance, h_fov, scale_height):
+
+    screen_array[:] = np.array([0, 0, 0])
+    y_buffer = np.full(screen_width, screen_height)
+
+    ray_angle = player_angle - h_fov
+    for num_ray in range(screen_width):
+        sin_a = math.sin(ray_angle)
+        cos_a = math.cos(ray_angle)
+
+        for depth in range(1, ray_distance):
+            x = int(player_pos[0] + depth * cos_a)
+            if 0 < x < map_width:
+                y = int(player_pos[1] + depth * sin_a)
+                if 0 < y < map_height:
+
+                    # remove fish eye
+                    depth *= math.cos(player_angle - ray_angle)
+                    height_on_screen = int((player_height - height_map[x, y][0]) /
+                                           depth * scale_height + player_pitch)
+
+                    # draw vert line
+                    if height_on_screen < y_buffer[num_ray]:
+                        for screen_y in range(height_on_screen, y_buffer[num_ray]):
+                            screen_array[num_ray, screen_y] = color_map[x, y]
+                        y_buffer[num_ray] = height_on_screen
+
+        ray_angle += delta_angle
+    return screen_array
+
+
 class VoxelRender:
     def __init__(self, app):
         self.app = app
@@ -26,7 +59,10 @@ class VoxelRender:
         self.screen_array = np.full((app.width, app.height, 3), (0, 0, 0))
 
     def update(self):
-        self.screen_array = np.random.randint(0, 255, size=self.screen_array.shape)
+        self.screen_array = ray_casting(self.screen_array, self.player.pos, self.player.angle,
+                                        self.player.height, self.player.pitch, self.app.width,
+                                        self.app.height, self.delta_angle, self.ray_distance,
+                                        self.h_fov, self.scale_height)
 
     def draw(self):
         self.app.screen.blit(pg.surfarray.make_surface(self.screen_array), (0, 0))
