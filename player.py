d2ad9c012d46758c5f2ae64530d30f9ac4091cb9
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-05-21 17:20:42 +0300

    add movement

--- player.py
+++ player.py
@@ -13,4 +13,34 @@ class Player:
         self.vel = 3
 
     def update(self):
-        pass
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+
+        pressed_key = pg.key.get_pressed()
+        if pressed_key[pg.K_UP]:
+            self.pitch += self.vel
+        if pressed_key[pg.K_DOWN]:
+            self.pitch -= self.vel
+
+        if pressed_key[pg.K_LEFT]:
+            self.pitch -= self.angle_vel
+        if pressed_key[pg.K_RIGHT]:
+            self.pitch += self.angle_vel
+
+        if pressed_key[pg.K_q]:
+            self.height += self.vel
+        if pressed_key[pg.K_e]:
+            self.height -= self.vel
+
+        if pressed_key[pg.K_w]:
+            self.pos[0] += self.vel * cos_a
+            self.pos[1] += self.vel * sin_a
+        if pressed_key[pg.K_s]:
+            self.pos[0] -= self.vel * cos_a
+            self.pos[1] -= self.vel * sin_a
+        if pressed_key[pg.K_a]:
+            self.pos[0] += self.vel * sin_a
+            self.pos[1] -= self.vel * cos_a
+        if pressed_key[pg.K_d]:
+            self.pos[0] -= self.vel * sin_a
+            self.pos[1] += self.vel * cos_a
